8ca636b8023d6d755001d5ec16b091efdc7e2a6a
adaSkribana <136006242+adaSkribana@users.noreply.github.com>
2024-02-02 16:09:18 +0000

    boton burguer

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+<script>
+/* Funciones para el botón de hamburguesa */
+
+    function myFunction(x) {
+        x.classList.toggle("change");
+    document.getElementById("myNav").style.width = "100%";
+    }
+
+</script>
